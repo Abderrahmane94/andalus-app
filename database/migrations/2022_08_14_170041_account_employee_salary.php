6676f282f0a172fce7fa8b76aa43d5f1218b0c86
Abderrahmane <abderrahmanesmailon20@gmail.com>
2022-09-24 18:19:37 +0100

    - commit new update!

---
+++ database/migrations/2022_08_14_170041_account_employee_salary.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AccountEmployeeSalary extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_employee_salary', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->integer('employee_id');
+            $table->double('principal_amount');
+            $table->double('grant_amount');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_employee_salary');
+    }
+}
